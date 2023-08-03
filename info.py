626d5e1839fa428d644ada8276c49c10ca58473f
aeditx01 <137967821+aeditx01@users.noreply.github.com>
2023-08-03 15:57:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -81,6 +81,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+CLOSE_IMG = (environ.get('CLOSE_IMG', 'https://telegra.ph/file/10654a2acb0bb4bd6c7fd.jpg')).split()
 
 LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
 
