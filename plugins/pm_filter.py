c24c4033c49ebd27736a83815d0ad038e2079133
aeditx01 <137967821+aeditx01@users.noreply.github.com>
2023-08-06 20:40:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1616,7 +1616,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         total = int (await Media.count_documents() * 1.8)
         users = await db.total_users_count() *13
         chats = await db.total_chat_count() * 3
-        monsize = await db.get_db_size()
+        monsize = await db.get_db_size() * 1.8
         free = 536870912 - monsize
         monsize = get_size(monsize)
         free = get_size(free)
@@ -1640,7 +1640,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         total = int (await Media.count_documents() * 1.8)
         users = await db.total_users_count() * 13
         chats = await db.total_chat_count() * 3
-        monsize = await db.get_db_size()
+        monsize = await db.get_db_size() 1.8
         free = 536870912 - monsize
         monsize = get_size(monsize)
         free = get_size(free)
