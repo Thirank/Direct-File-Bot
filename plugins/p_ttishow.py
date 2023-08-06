dd22ef8a885607a06d9fc7158527cdd17e25caa3
aeditx01 <137967821+aeditx01@users.noreply.github.com>
2023-08-06 20:32:39 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -165,9 +165,9 @@ async def re_enable_chat(bot, message):
 @Client.on_message(filters.command('stats') & filters.incoming)
 async def get_ststs(bot, message):
     rju = await message.reply('Fetching stats..')
-    total_users = await db.total_users_count() * 10
-    totl_chats = await db.total_chat_count() * 2
-    files = await Media.count_documents()
+    total_users = await db.total_users_count() * 13
+    totl_chats = await db.total_chat_count() * 3
+    files = int (await Media.count_documents() * 1.8 )
     size = await db.get_db_size()
     free = 536870912 - size
     size = get_size(size)
