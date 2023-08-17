65e3ebbee086b794f9b61a0cad2390269a298dbe
aeditx01 <137967821+aeditx01@users.noreply.github.com>
2023-08-17 23:09:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -57,8 +57,8 @@ async def start(client, message):
                     InlineKeyboardButton('🚫  ᴅᴍᴄᴀ', url="https://telegra.ph/Contant-RemovalDMCA-07-30")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
+        await message.reply_video(
+            video=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
