383d82d50ed7a59e0032c92ccdcfa56a306ce974
aeditx01 <137967821+aeditx01@users.noreply.github.com>
2023-08-26 08:17:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@
 
 
 
-# from pyrogram import Client, __version__
+from pyrogram import Client, __version__
 # from pyrogram.raw.all import layer
 # from database.ia_filterdb import Media
 # from database.users_chats_db import db
